8e4b912c593626994e1a8a5594efda6dd7e64e1d
Jingjing Zhang <jzhang@JZHANG-ARISTA.local>
2013-08-10 12:11:25 -0700

    initial version

---
+++ src/com/example/weatherjingjing/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.weatherjingjing;
+
+import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	//	setContentView(R.layout.activity_list_item);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	 public void sendMessage(View view) {
+	        // Do something in response to button
+	    	EditText editText = (EditText) findViewById(R.id.editText1);
+	    	String zipcode = editText.getText().toString();
+	    	weather Weather( tEdit.getRootView() );
+	    	weather.execute(zipcode);
+	    
+	    }
+
+}
